e1238c480dec4215a31c8ebda0e22a886994738a
JoshuaT <jttsat170@gmail.com>
2026-03-22 21:17:46 -0400

    Working on the springboot application, there were some current issues will come back to later.

---
+++ projects/grocery-store-management-system/src/main/java/com/grocery/GroceryStoreApp.java
@@ -0,0 +1,15 @@
+package com.grocery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class GroceryStoreApp {
+    public static void main(String[] args) {
+        SpringApplication.run(GroceryStoreApp.class, args);
+    }
+}
+
+
+
